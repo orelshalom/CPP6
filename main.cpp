17233ef80d42f172087bb98eb914b24135e257b1
Orel Shalom <33144903+orelshalom@users.noreply.github.com>
2018-05-22 17:06:04 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "TicTacToe.h"
 #include "DummyPlayers.h"
-// #include "Champion.h"
+#include "Champion.h"
 
 void printResults(const TicTacToe& game) {
 	cout << endl << "The final board is " << endl << game.board();
