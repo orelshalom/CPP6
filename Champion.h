e2053579300474e97db845de79e091b94013a66b
Orel Shalom <33144903+orelshalom@users.noreply.github.com>
2018-05-22 17:07:44 +0300

    Add files via upload

--- Champion.h
+++ Champion.h
@@ -2,7 +2,7 @@
 #include "Player.h"
 
 class Champion: public Player {
-
+    public:
     const string name() const override { return "Orel Shalom, Naama Shtauber"; }
 	const Coordinate play(const Board& board) override;
     
